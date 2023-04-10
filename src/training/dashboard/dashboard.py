6ea4a74dee4e0b06849d28ddfa24580059067538
Florian Hocquet <f.hocquet@aimpower.ai>
2023-04-10 03:50:55 +0200

    first version

--- src/training/dashboard/dashboard.py
+++ src/training/dashboard/dashboard.py
@@ -160,11 +160,8 @@ class Dashboard:
             f"target_{step}": utils.adjust_image_colors(target_tensor).permute(1, 2, 0),
             f"pred_{step}": utils.adjust_image_colors(pred_tensor).permute(1, 2, 0),
         }
-
-        torchvision.utils.save_image(images["image_train"].type(torch.float32), "image.png")
-        torchvision.utils.save_image(images["input_train"].type(torch.float32), "input.png")
-        torchvision.utils.save_image(images["target_train"].type(torch.float32), "target.png")
-        torchvision.utils.save_image(images["pred_train"].type(torch.float32), "pred.png")
+        print(image.shape)
+        print(images["image_train"].shape)
 
         for image_id in images.keys():
             images[image_id] = [wandb.Image(self._tensor_to_pil(images[image_id]))]
