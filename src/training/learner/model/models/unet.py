094fb061726bd5697004b164fe805e68c1abfc7a
Florian Hocquet <f.hocquet@aimpower.ai>
2023-04-14 11:33:47 +0200

    not important

--- src/training/learner/model/models/unet.py
+++ src/training/learner/model/models/unet.py
@@ -21,7 +21,8 @@ class UNet(UNet2DModel):
             img_size: int,
             in_channels: int,
             out_channels: int,
-            block_out_channels: Tuple[int]
+            block_out_channels: Tuple[int],
+            class_embed_type: Optional[str] = None
     ):
         """
         Instantiates a UNet.
@@ -42,19 +43,13 @@ class UNet(UNet2DModel):
             sample_size=img_size,
             in_channels=in_channels, out_channels=out_channels,
             layers_per_block=2, block_out_channels=block_out_channels,
-
             down_block_types=(
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",
-                "DownBlock2D",
+                "DownBlock2D", "DownBlock2D", "AttnDownBlock2D", "DownBlock2D",
             ),
             up_block_types=(
-                "UpBlock2D",
-                "AttnUpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D"
+                "UpBlock2D", "AttnUpBlock2D", "UpBlock2D", "UpBlock2D"
             ),
+            class_embed_type=class_embed_type,
         )
 
     def __str__(
