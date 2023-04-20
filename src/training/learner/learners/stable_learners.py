3380b2dfc3da0e28f63ece85a4ee4f2d5b4bf2fb
Florian Hocquet <f.hocquet@aimpower.ai>
2023-04-20 14:35:43 +0200

    not important

--- src/training/learner/learners/stable_learners.py
+++ src/training/learner/learners/stable_learners.py
@@ -241,7 +241,7 @@ class ConditionedStableLearner(StableLearner):
         """
         # Pipeline
         pipeline = diffusers.DiffusionPipeline.from_pretrained(
-            pretrained_model_name_or_path=self._params["components"]["model"]["pipeline_path"],
+            pretrained_model_name_or_path=self._params["components"]["vae"]["pipeline_path"],
             unet=self.components.model
         ).to(self._DEVICE)
 
