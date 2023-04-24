f52c8ea9844f36e401a8d6606f6a1afb92498822
Florian Hocquet <f.hocquet@aimpower.ai>
2023-04-24 02:03:48 +0200

    not important

--- src/training/learner/components/diffusion_components.py
+++ src/training/learner/components/diffusion_components.py
@@ -134,7 +134,7 @@ class DiffusionComponents:
                 pretrained_model_name_or_path=self._params["pipeline_path"],
                 subfolder="unet",
                 revision="fp16",
-                low_cpu_mem_usage=False
+                low_cpu_mem_usage=True
             )
 
         self.model = self._M_TYPES[self._params["model"]["type"]](**self._params["model"]["args"])
