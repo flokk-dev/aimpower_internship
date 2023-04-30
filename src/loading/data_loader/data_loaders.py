e7c9f07542b86f50d325ad1e18c01a6eca2a18b4
Florian Hocquet <f.hocquet@aimpower.ai>
2023-04-30 20:22:23 +0200

    not important

--- src/loading/data_loader/data_loaders.py
+++ src/loading/data_loader/data_loaders.py
@@ -146,7 +146,7 @@ class PromptDataLoader(DataLoader):
                 [e["image"] for e in data]
             ).to(
                 memory_format=torch.contiguous_format
-            ).type(torch.float16 if self._params["fp16"] else torch.float32),
+            ).type(torch.float16 if self._params["dtype"] == "fp16" else torch.float32),
 
             "prompt": self.dataset.tokenizer.pad(
                 {"input_ids": [e["prompt"] for e in data]}, padding=True, return_tensors="pt"
